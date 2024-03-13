47ec245fbadefd7c1b77e6d47c6ed00a1f158a54
josephgeraldo <99371640+grald003@users.noreply.github.com>
2024-03-13 20:15:29 +0700

    Fixed the Layout of Navbar

--- disetiratasan/src/app/carview/layout.tsx
+++ disetiratasan/src/app/carview/layout.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import Footer from "@/components/Utilities/Footer";
+import Navbar from "@/components/Utilities/Navbar";
 import { useRouter } from "next/navigation";
 import React from "react";
 
@@ -12,38 +14,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
   return (
     <div>
-      <header className="bg-header py-4">
-        <div className="container mx-auto flex justify-between items-center">
-          <div className="flex items-center">
-              <img
-                onClick={()=>onClickUrl('/home')}
-                className="cursor-pointer"
-                src="Logo.png"
-                width={100}
-                height={100}
-                alt="Disetir Atasan Logo"
-              />
-            <div className="border-l border-white h-14 ml-4"></div>
-            <nav className="ml-4">
-              <ul className="flex space-x-4">
-                <li className="text-white hover:text-gray-300 cursor-pointer" onClick={()=>onClickUrl('/home')}>
-                    Home
-                </li>
-                <li className="text-white hover:text-gray-300 cursor-pointer" onClick={()=>onClickUrl('/vehicle')}>
-                    Vehicles
-                </li>
-                <li className="text-white hover:text-gray-300 cursor-pointer" onClick={()=>onClickUrl('/about')}>
-                    About Us
-                </li>
-              </ul>
-            </nav>
-          </div>
-          <div className="text-white hover:text-gray-300 cursor-pointer" onClick={()=>onClickUrl('/login')}>
-              Login
-          </div>
-        </div>
-      </header>
+      <Navbar/>
       {children}
+      <Footer/>
     </div>
   );
 }
