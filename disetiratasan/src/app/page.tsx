315e51f93510d705103b0e445ab430cc1e1ae909
josephgeraldo <99371640+grald003@users.noreply.github.com>
2024-03-19 14:26:59 +0700

    Finished Vehicle List Design

--- disetiratasan/src/app/page.tsx
+++ disetiratasan/src/app/page.tsx
@@ -6,17 +6,17 @@ import Hover from "@/components/WelcomeText";
 export default function Home() {
   return (
     <div className="max-w-1440 m-auto">
-      <Hover/>
+      <Hover />
       <div className="font-bold md:text-lg text-md bg-blue-new flex item-center text-white w-60 p-1 pl-2 ml-3 mt-3">
         <h1>New Arrivals</h1>
       </div>
       <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-2 gap-3 h-1/2 p-3">
-        <NewArrivals/>
+        <NewArrivals />
       </div>
       <div className="p-3">
-        <BuildCustom/>
+        <BuildCustom />
       </div>
-      <Trending/>
+      <Trending />
     </div>
   );
 }
