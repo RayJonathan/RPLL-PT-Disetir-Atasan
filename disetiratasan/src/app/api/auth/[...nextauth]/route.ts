7fa51c0514a3c5fec2f645d0a9410f736e401fa6
Ray Jonathan <rayjonathan5415@gmail.com>
2024-03-25 12:54:20 +0700

    Create route.ts

---
+++ disetiratasan/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,26 @@
+import NextAuth, { NextAuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+export const authOptions: NextAuthOptions = {
+    session: {
+        strategy: 'jwt'
+    },
+
+    providers: [
+        CredentialsProvider({
+            name: 'Sign in',
+            credentials: {
+                // Fix the types for email and password
+                email: { label: 'Email', type: 'email', placeholder: 'hello@example.com' },
+                password: { label: 'Password', type: 'password' }
+            },
+            async authorize(credentials) {
+                // Correctly return a User object
+                const user = { id: 1, name: 'Ethan', email: 'test@test.com' }
+                return user
+            }
+        })
+    ]
+}
+const handler = NextAuth(authOptions)
+export { handler as GET, handler as POST }
