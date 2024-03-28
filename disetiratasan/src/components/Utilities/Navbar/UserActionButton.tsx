317de2287f043e178394fb887d75c4d2b4e5f604
josephgeraldo <99371640+grald003@users.noreply.github.com>
2024-03-28 10:17:39 +0700

    Update UserActionButton

--- disetiratasan/src/components/Utilities/Navbar/UserActionButton.tsx
+++ disetiratasan/src/components/Utilities/Navbar/UserActionButton.tsx
@@ -1,14 +1,17 @@
 import { authUserSession } from "@/libs/auth-libs";
 import Link from "next/link";
 
-const UserActionButton = async() => {
+const UserActionButton = async () => {
     const user = await authUserSession();
     const actionLabel = user ? "Sign Out" : "Sign In"
     const actionURL = user ? "/api/auth/signout" : "/login"
+    const myAccount = user ? "My Account" : ""
+    const myAccountURL = user ? "/" : ""
 
-    return(
-        <div>
-            <Link href={actionURL}>{actionLabel}</Link>
+    return (
+        <div className="flex gap-4">
+            <Link href={myAccountURL} className="">{myAccount}</Link>
+            <Link href={actionURL} className="">{actionLabel}</Link>
         </div>
     )
 }
